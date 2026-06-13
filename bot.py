80c2b7e681be0f389632ffb45e643f270fa57f6e
zaynlior05-maker <zaynlior05@gmail.com>
2026-06-13 01:18:18 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,13 +57,11 @@ logger = logging.getLogger(__name__)
 # ============================================================
 def welcome_text() -> str:
     return (
-        f"\U0001F539 Support account is available 24/7 {config.SUPPORT_HANDLE}\n"
-        "\U0001F539\n"
-        "\U0001F539 <b>BY PURCHASING YOU AGREE TO THESE RULES. "
-        "FAILURE TO READ THEM WILL FORFEIT YOUR REFUND / REPLACEMENT. "
-        "WE SHALL GIVE NO WARNINGS</b>\n\n"
-        "Welcome to the Store \U0001F44B\n"
-        "Use the menu below to interact with the bot \U0001F916"
+        "Welcome to EXCEL Store 👋\n"
+        "Use the menu below to interact with the bot 🤖\n"
+        "===============\n"
+        "Managed by @EXCELV1\n"
+        "Coded by @EXCELV1 on session 05a5c62989edb4dadf7cb1274e35e37d498b5af459b04e08fe08ab037a206ec841"
     )
 
 
